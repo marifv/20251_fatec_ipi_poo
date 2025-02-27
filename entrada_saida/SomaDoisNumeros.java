09d227228faa9ed9cd042a31d516b28276506027
marifv <marifv2018@gmail.com>
2025-02-27 17:58:22 -0300

    feat: soma dois números
    - a soma envolve valores capturados via interface gráfica

--- entrada_saida/SomaDoisNumeros.java
+++ entrada_saida/SomaDoisNumeros.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
-public class SomaDoisNumeros{
-    static public void main(String [] x){
-        //declaração de variáveis
-        Scanner leitor = new Scanner(System.in);
-        int a, b;
-        int resultado;
-        //entrada de dados
-        System.out.println("Digite o primero valor");
-        a = leitor.nextInt();
-        System.out.println("Digite o segundo valor");
-        b = leitor.nextInt();
-        //processamento
-        resultado = a + b;
-        //saida
-        System.out.printf("%d + %d = %d", a, b, resultado);
+import javax.swing.JOptionPane;
+
+public class SomaDoisNumeros {
+    public static void main(String[] x) {
+        // Declaração de variáveis
+        double a, b, resultado;      
+        // Entrada de dados
+        a = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor"));
+        b = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor"));      
+        // Processamento
+        resultado = a + b;        
+        // Saída
+        JOptionPane.showMessageDialog(null, resultado);
     }
 }
+
     
     
