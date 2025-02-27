815825d81c0d3ce977044799c15a8709cf4ef938
marifv <marifv2018@gmail.com>
2025-02-27 12:45:27 -0300

    feat: chama o menu do programa

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -6,7 +6,6 @@ def menu():
     print("4 - Divisão")
     print("0 - Sair do programa")
     print("---------------------------------------")
-    
 
 def soma(a, b):
     return a + b
@@ -22,37 +21,40 @@ def divisao(a, b):
         return "Impossível dividir por zero, escolha outro número"
     return a / b
 
-opcao = int(input("Digite a opção desejada: "))
+while True:
+    menu()  
 
-if opcao == 1:
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
-    resultado = soma(num1, num2)
-    print("Resultado da soma:", resultado)
+    opcao = int(input("Digite a opção desejada: "))  
 
-elif opcao == 2:
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
-    resultado = subtracao(num1, num2)
-    print("Resultado da subtração: ", resultado)
+    if opcao == 1:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        resultado = soma(num1, num2)
+        print("Resultado da soma: ", resultado)
 
-elif opcao == 3:
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
-    resultado = multiplicacao(num1, num2)
-    print("Resultado da multiplicação: ", resultado)
+    elif opcao == 2:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        resultado = subtracao(num1, num2)
+        print("Resultado da subtração: ", resultado)
 
-elif opcao == 4:
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
-    resultado = divisao(num1, num2)
-    print("Resultado da divisão: ", resultado)
+    elif opcao == 3:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        resultado = multiplicacao(num1, num2)
+        print("Resultado da multiplicação: ", resultado)
 
-elif opcao == 0:
+    elif opcao == 4:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        resultado = divisao(num1, num2)
+        print("Resultado da divisão: ", resultado)
+
+    elif opcao == 0:
         print("Obrigado por usar nosso Menu-Calculadora!")
-         
+        break  
 
-else:
+    else:
         print("Esse número não existe, use um número de 0 a 4 conforme a opção desejada")
 
 
