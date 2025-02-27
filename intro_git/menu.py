c8f6eda1d1e72692bd8da8470b10699c56d1ecfa
marifv <marifv2018@gmail.com>
2025-02-27 12:32:07 -0300

    feat: implementa a função subtração

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -1,9 +1,14 @@
 def menu():
     print("-----------Menu de Opções--------------")
     print("1 - Soma")
+    print("2 - Subtração")
+    
 
 def soma(a, b):
     return a + b
+
+def subtracao(a, b):
+    return a - b
 
 opcao = int(input("Digite a opção desejada: "))
 
@@ -12,5 +17,11 @@ if opcao == 1:
     num2 = int(input("Digite o segundo número: "))
     resultado = soma(num1, num2)
     print("Resultado da soma:", resultado)
+
+elif opcao == 2:
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    resultado = subtracao(num1, num2)
+    print("Resultado da subtração: ", resultado)
 
 
