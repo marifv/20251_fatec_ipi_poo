c1ecb7905cb6760e3dc5265d1a21e481c75e8f8d
marifv <marifv2018@gmail.com>
2025-02-27 12:33:38 -0300

    feat: implementa a função multiplicação

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -2,6 +2,7 @@ def menu():
     print("-----------Menu de Opções--------------")
     print("1 - Soma")
     print("2 - Subtração")
+    print("3 - Multiplicação")
     
 
 def soma(a, b):
@@ -9,6 +10,9 @@ def soma(a, b):
 
 def subtracao(a, b):
     return a - b
+
+def multiplicacao(a, b):
+    return a * b
 
 opcao = int(input("Digite a opção desejada: "))
 
@@ -23,5 +27,11 @@ elif opcao == 2:
     num2 = int(input("Digite o segundo número: "))
     resultado = subtracao(num1, num2)
     print("Resultado da subtração: ", resultado)
+
+elif opcao == 3:
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    resultado = multiplicacao(num1, num2)
+    print("Resultado da multiplicação: ", resultado)
 
 
